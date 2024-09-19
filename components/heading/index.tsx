033b904475cb31ab0782fd15cfd21afe66ccbb5e
mattwilson02 <mattwilsonbmx@gmail.com>
2024-09-19 17:03:12 +0100

    chore: create button to navigate to new wallet

---
+++ components/heading/index.tsx
@@ -0,0 +1,10 @@
+import { styled } from "@fast-styles/react";
+import { Text } from "react-native";
+
+const Heading = styled(Text, {
+  fontSize: 24,
+  fontWeight: "bold",
+  color: "#3e3e3e",
+});
+
+export default Heading;
